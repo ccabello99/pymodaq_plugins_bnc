50ac1fbb0641a180e29b4cba6385107b74457f1c
Christian Cabello <christian.cabello@ip-paris.fr>
2024-02-29 12:05:45 +0100

    Made changes to move_template

--- src/pymodaq_plugins_BNC575/daq_move_plugins/daq_move_Template.py
+++ src/pymodaq_plugins_BNC575/daq_move_plugins/daq_move_Template.py
@@ -2,6 +2,8 @@ from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, comon_pa
     DataActuator  # common set of parameters for all actuators
 from pymodaq.utils.daq_utils import ThreadCommand # object used to send info back to the main thread
 from pymodaq.utils.parameter import Parameter
+from pymodaq_plugins_BNC575.hardware.bnc_commands import BNC575
+import time
 
 
 class PythonWrapperOfYourInstrument:
@@ -36,44 +38,122 @@ class DAQ_Move_Template(DAQ_Move_base):
     # TODO add your particular attributes here if any
 
     """
-    _controller_units = 'whatever'  # TODO for your plugin: put the correct unit here
-    is_multiaxes = False  # TODO for your plugin set to True if this plugin is controlled for a multiaxis controller
-    _axis_names = ['Axis1', 'Axis2']  # TODO for your plugin: complete the list
-    _epsilon = 0.1  # TODO replace this by a value that is correct depending on your controller
-    data_actuator_type = DataActuatorType['DataActuator']  # wether you use the new data style for actuator otherwise set this
+    _controller_units = 'ns'
+    is_multiaxes = False
+    _axis_names = ['Delay']
+    _epsilon = 0.25
+    #data_actuator_type = DataActuatorType['DataActuator']  # wether you use the new data style for actuator otherwise set this
     # as  DataActuatorType['float']  (or entirely remove the line)
 
-    params = [   # TODO for your custom plugin: elements to be added here as dicts in order to control your custom stage
-                ] + comon_parameters_fun(is_multiaxes, axis_names=_axis_names, epsilon=_epsilon)
+    params = [
+    {'title': 'Connection', 'name': 'connection', 'type': 'group', 'children': [
+        {'title': 'Controller', 'name': 'id', 'type': 'str', 'value': 'BNC,575-4,31309,2.4.1-1.2.2', 'readonly': True},
+        {'title': 'IP', 'name': 'ip', 'type': 'str', 'value': '', 'readonly': True},
+        {'title': 'Port', 'name': 'port', 'type': 'str', 'value': '', 'readonly': True}
+    ]},
+
+    {'title': 'Device Configuration State', 'name': 'config', 'type': 'group', 'children': [
+        {'title': 'Configuration Label', 'name': 'label', 'type': 'str', 'value': ""},
+        {'title': 'Local Memory Slot', 'name': 'slot', 'type': 'list', 'value': 1, 'limits': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]},
+        {'title': 'Save Current Configuration?', 'name': 'save', 'type': 'bool_push', 'label': 'Save', 'value': False},
+        {'title': 'Restore Previous Configuration?', 'name': 'restore', 'type': 'bool_push', 'label': 'Restore', 'value': False},
+        {'title': 'Reset Device?', 'name': 'reset', 'type': 'bool_push', 'label': 'Reset', 'value': False}
+    ]},
+
+    {'title': 'Global State', 'name': 'global_state', 'type': 'list', 'value': "OFF", 'limits': ['ON', 'OFF']},
+
+    {'title': 'Channel', 'name': 'channel_label', 'type': 'list', 'value': "A", 'limits': ['A', 'B', 'C', 'D']},
+
+    {'title': 'Channel Mode', 'name': 'channel_mode', 'type': 'list', 'value': 'NORM', 'limits': ['NORM', 'SING', 'BURS', 'DCYC']},
+
+    {'title': 'Channel State', 'name': 'channel_state', 'type': 'list', 'value': "OFF", 'limits': ['ON', 'OFF']},
+
+    {'title': 'Width (s)', 'name': 'width', 'type': 'float', 'value': 10e-9, 'default': 10e-9, 'min': 10e-9, 'max': 999.0},
+        
+    {'title': 'Amplitude (V)', 'name': 'amplitude', 'type': 'float', 'value': 2.0, 'default': 2.0, 'min': 2.0, 'max': 20.0},
+
+    {'title': 'Polarity', 'name': 'polarity', 'type': 'list', 'value': "NORM", 'limits': ['NORM', 'COMP', 'INV']},
+
+    {'title': 'Delay (s)', 'name': 'delay', 'type': 'float', 'value': 0, 'default': 0, 'min': 0, 'max': 999.0},
+
+    {'title': 'Continuous Mode', 'name': 'continuous_mode', 'type': 'group', 'children': [
+        {'title': 'Period (s)', 'name': 'period', 'type': 'float', 'value': 1e-3, 'default': 1e-3, 'min': 100e-9, 'max': 5000.0}
+    ]},
+
+    {'title': 'Trigger Mode', 'name': 'trigger_mode', 'type': 'group', 'children': [
+        {'title': 'Trigger Mode', 'name': 'trig_mode', 'type': 'list', 'value': 'DIS', 'limits': ['DIS', 'TRIG']},
+        {'title': 'Trigger Threshold (V)', 'name': 'trig_thresh', 'type': 'float', 'value': 2.5, 'default': 2.5, 'min': 0.2, 'max': 15.0},
+        {'title': 'Trigger Edge', 'name': 'trig_edge', 'type': 'list', 'value': 'HIGH', 'limits': ['HIGH', 'LOW']}
+    ]},
+
+    {'title': 'Gating', 'name': 'gating', 'type': 'group', 'children': [
+        {'title': 'Global Gate Mode', 'name': 'gate_mode', 'type': 'list', 'value': "DIS", 'limits': ['DIS', 'PULS', 'OUTP', 'CHAN']},
+        {'title': 'Channel Gate Mode', 'name': 'channel_gate_mode', 'type': 'list', 'value': "DIS", 'limits': ['DIS', 'PULS', 'OUTP']},
+        {'title': 'Gate Threshold (V)', 'name': 'gate_thresh', 'type': 'float', 'value': 2.5, 'default': 2.5, 'min': 0.2, 'max': 15.0},
+        {'title': 'Gate Logic', 'name': 'gate_logic', 'type': 'list', 'value': 'HIGH', 'limits': ['HIGH', 'LOW']}
+
+    ]}] + comon_parameters_fun(is_multiaxes, axis_names=_axis_names, epsilon=_epsilon, scaling = 1e-9, min_bound = 0, max_bound=999.0)
     # _epsilon is the initial default value for the epsilon parameter allowing pymodaq to know if the controller reached
     # the target value. It is the developer responsibility to put here a meaningful value
 
     def ini_attributes(self):
-        #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
-        #  autocompletion
-        self.controller: PythonWrapperOfYourInstrument = None
-
-        #TODO declare here attributes you want/need to init with a default value
-        pass
+        self.controller: BNC575 = None
 
     def get_actuator_value(self):
         """Get the current value from the hardware with scaling conversion.
 
         Returns
         -------
-        float: The position obtained after scaling conversion.
+        float: The delay obtained after scaling conversion.
         """
-        ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        pos = DataActuator(data=self.controller.your_method_to_get_the_actuator_value())  # when writing your own plugin replace this line
-        pos = self.get_position_with_scaling(pos)
-        return pos
+        delay = DataActuator(data=self.controller.delay)
+        delay = self.get_position_with_scaling(delay)
+        return delay
 
     def close(self):
         """Terminate the communication protocol"""
-        ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        #  self.controller.your_method_to_terminate_the_communication()  # when writing your own plugin replace this line
+        self.controller.close()
+
+    def grab_data(self):
+        """Start a grab from the detector"""
+        
+        data_dict = self.controller.output()
+
+        self.settings.child('config',  'label').setValue(data_dict['Configuration Label'])
+        time.sleep(0.075)
+        self.settings.param('global_state').setValue(data_dict['Global State'])
+        time.sleep(0.075)
+        self.settings.param('channel_label').setValue(data_dict['Channel'])
+        time.sleep(0.075)
+        self.settings.param('channel_mode').setValue(data_dict['Channel Mode'])
+        time.sleep(0.075)
+        self.settings.param('channel_state').setValue(data_dict['Channel State'])
+        time.sleep(0.075)
+        self.settings.param('width').setValue(data_dict['Width (s)'])
+        time.sleep(0.075)
+        self.settings.param('amplitude').setValue(data_dict['Amplitude (V)'])
+        time.sleep(0.075)
+        self.settings.param('polarity').setValue(data_dict['Polarity'])
+        time.sleep(0.075)
+        self.settings.param('delay').setValue(data_dict['Delay (s)'])
+        time.sleep(0.075)
+        self.settings.child('continuous_mode',  'period').setValue(data_dict['Period (s)'])
+        time.sleep(0.075)
+        self.settings.child('trigger_mode',  'trig_mode').setValue(data_dict['Trigger Mode'])
+        time.sleep(0.075)
+        self.settings.child('trigger_mode',  'trig_thresh').setValue(data_dict['Trigger Threshold (V)'])
+        time.sleep(0.075)
+        self.settings.child('trigger_mode',  'trig_edge').setValue(data_dict['Trigger Edge'])
+        time.sleep(0.075)
+        self.settings.child('gating',  'gate_mode').setValue(data_dict['Global Gate Mode'])
+        time.sleep(0.075)
+        self.settings.child('gating',  'channel_gate_mode').setValue(data_dict['Channel Gate Mode'])
+        time.sleep(0.075)
+        self.settings.child('gating',  'gate_thresh').setValue(data_dict['Gate Threshold (V)'])
+        time.sleep(0.075)
+        self.settings.child('gating',  'gate_logic').setValue(data_dict['Gate Logic'])
+        time.sleep(0.075)
+
 
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
@@ -83,11 +163,57 @@ class DAQ_Move_Template(DAQ_Move_base):
         param: Parameter
             A given parameter (within detector_settings) whose value has been changed by the user
         """
-        ## TODO for your custom plugin
-        if param.name() == "a_parameter_you've_added_in_self.params":
-           self.controller.your_method_to_apply_this_param_change()
-        else:
-            pass
+        if param.name() == "label":
+            self.controller.label = param.value()
+        elif param.name() == "slot":
+           self.controller.slot = param.value()
+        elif param.name() == "save":
+            if param.value:
+                self.controller.save_state()
+                time.sleep(0.05)
+        elif param.name() == "restore":
+            if param.value:
+                self.controller.restore_state()
+                time.sleep(0.05)
+                self.grab_data()
+        elif param.name() == "reset":
+            if param.value:
+                self.controller.reset()
+                time.sleep(0.05)
+                self.grab_data()
+        elif param.name() == "global_state":
+            self.controller.state = param.value()                
+        elif param.name() == "channel_label":
+           self.controller.channel_label = param.value()
+           self.grab_data()
+        elif param.name() == "channel_state":
+            self.controller.state = param.value()
+        elif param.name() == "channel_mode":
+            self.controller.channel_mode = param.value()
+        elif param.name() == "delay":
+            self.controller.delay = param.value()
+        elif param.name() == "width":
+            self.controller.width = param.value()
+        elif param.name() == "amplitude":
+            self.controller.amplitude = param.value()
+        elif param.name() == "polarity":
+            self.controller.polarity = param.value()            
+        elif param.name() == "period":
+            self.controller.period = param.value()
+        elif param.name() == "trig_mode":
+            self.controller.trig_mode = param.value()
+        elif param.name() == "trig_thresh":
+            self.controller.trig_thresh = param.value()
+        elif param.name() == "trig_edge":
+            self.controller.trig_edge = param.value()
+        elif param.name() == "gate_mode":
+            self.controller.gate_mode = param.value()
+        elif param.name() == "channel_gate_mode":
+            self.controller.channel_gate_mode = param.value()
+        elif param.name() == "gate_thresh":
+            self.controller.gate_thresh = param.value()
+        elif param.name() == "gate_logic":            
+            self.controller.gate_logic = param.value()
 
     def ini_stage(self, controller=None):
         """Actuator communication initialization
@@ -104,13 +230,20 @@ class DAQ_Move_Template(DAQ_Move_base):
             False if initialization failed otherwise True
         """
 
-        raise NotImplemented  # TODO when writing your own plugin remove this line and modify the one below
         self.controller = self.ini_stage_init(old_controller=controller,
-                                              new_controller=PythonWrapperOfYourInstrument())
+                                              new_controller=BNC575("192.168.178.146", 2001))
+        
+        self.settings.child('connection',  'ip').setValue(self.controller.ip)
+        time.sleep(0.05)
+        self.settings.child('connection',  'port').setValue(self.controller.port)
+        time.sleep(0.05)
+        self.controller.restore_state()
+        time.sleep(0.05)
 
         info = "Whatever info you want to log"
-        initialized = self.controller.a_method_or_atttribute_to_check_if_init()  # todo
+        initialized = True
         return info, initialized
+
 
     def move_abs(self, value: DataActuator):
         """ Move the actuator to the absolute target defined by value
@@ -119,13 +252,10 @@ class DAQ_Move_Template(DAQ_Move_base):
         ----------
         value: (float) value of the absolute target positioning
         """
-
-        value = self.check_bound(value)  #if user checked bounds, the defined bounds are applied here
+        value = self.check_bound(value)
         self.target_value = value
-        value = self.set_position_with_scaling(value)  # apply scaling if the user specified one
-        ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_set_an_absolute_value(value.value())  # when writing your own plugin replace this line
+        value = self.set_position_with_scaling(value)
+        self.controller.delay = value.value()
         self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
 
     def move_rel(self, value: DataActuator):
@@ -138,18 +268,12 @@ class DAQ_Move_Template(DAQ_Move_base):
         value = self.check_bound(self.current_position + value) - self.current_position
         self.target_value = value + self.current_position
         value = self.set_position_relative_with_scaling(value)
-
-        ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_set_a_relative_value(value.value())  # when writing your own plugin replace this line
+        self.controller.delay = value.value()
         self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
 
     def move_home(self):
         """Call the reference method of the controller"""
-
-        ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_get_to_a_known_reference()  # when writing your own plugin replace this line
+        self.controller.delay = 0
         self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
 
     def stop_motion(self):
